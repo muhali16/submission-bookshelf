8782457bf9382dead1f67512d38ca55082c31b8f
Ali <muhammadali55214@gmail.com>
2021-08-11 17:06:15 +0700

    update final

--- src/getBooks.js
+++ src/getBooks.js
@@ -1,5 +1,51 @@
 const books = require('./book');
 const getBook = (request, h) => {
+  const {reading, finished, name} = request.query;
+  if (reading) {
+    const bookFil = books.filter((b) => Number(b.reading) === Number(reading));
+    const response = h.response({
+      status: 'success',
+      data: {
+        books: bookFil.map((itm) => ({
+          id: itm.id,
+          name: itm.name,
+          publisher: itm.publisher,
+        })),
+      },
+    });
+    response.code(200);
+    return response;
+  } else if (finished) {
+    const bookFil = books.filter((b) => Number(b.finished)===Number(finished));
+    const response = h.response({
+      status: 'success',
+      data: {
+        books: bookFil.map((itm) => ({
+          id: itm.id,
+          name: itm.name,
+          publisher: itm.publisher,
+        })),
+      },
+    });
+    response.code(200);
+    return response;
+  } else if (name) {
+    const bookFil = books.filter((b) => {
+      return b.name.toLowerCase().includes(name.toLowerCase());
+    });
+    const response = h.response({
+      status: 'success',
+      data: {
+        books: bookFil.map((itm) => ({
+          id: itm.id,
+          name: itm.name,
+          publisher: itm.publisher,
+        })),
+      },
+    });
+    response.code(200);
+    return response;
+  }
   const response = h.response({
     status: 'success',
     data: {
